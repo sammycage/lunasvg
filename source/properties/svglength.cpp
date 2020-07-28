6ac2231db0af53012cc6f58a6c560d5499199cef
Ryo Suzuki <reputeless+github@gmail.com>
2020-07-29 00:11:25 +0900

    Enable MSVC math constants

--- source/properties/svglength.cpp
+++ source/properties/svglength.cpp
@@ -1,3 +1,8 @@
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
+
 #include "svglength.h"
 #include "rendercontext.h"
 #include "svgelementimpl.h"
--- source/properties/svgtransform.cpp
+++ source/properties/svgtransform.cpp
@@ -1,3 +1,8 @@
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
+
 #include "svgtransform.h"
 
 namespace lunasvg {
