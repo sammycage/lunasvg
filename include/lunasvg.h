906d326b29177a175200d4189c3858f68b4e401a
sammycage <sammycage8051@gmail.com>
2023-05-26 10:22:28 +0100

    Revert "Merge pull request #84 from seanharmer/master"
    This reverts commit b7e72fb377084883f9e672c045c91428860f9f20, reversing
    changes made to 4c16abf416ff07bee96ccf838cd2172c429a5dec.

--- include/lunasvg.h
+++ include/lunasvg.h
@@ -27,8 +27,15 @@
 #include <string>
 #include <cstdint>
 
-#include <lunasvg_export.h>
-#define LUNASVG_API LUNASVG_EXPORT
+#if defined(_MSC_VER) && defined(LUNASVG_SHARED)
+#ifdef LUNASVG_EXPORT
+#define LUNASVG_API __declspec(dllexport)
+#else
+#define LUNASVG_API __declspec(dllimport)
+#endif
+#else
+#define LUNASVG_API
+#endif
 
 namespace lunasvg {
 
